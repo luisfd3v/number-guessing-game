ae022906af6fffef6e6c389356535f1f968ac374
luisfd3v <luisfd3v@gmail.com>
2026-02-19 15:05:23 -0300

    feat: add random number

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("I'm thinking of a number between 1 and 100.");
 Console.WriteLine("You have some chances to guess the correct number.");
 
+Random random = new Random();
+int randomNumber = random.Next(1, 101);
